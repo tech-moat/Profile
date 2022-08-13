fe188d1dc049df63b38171f8951b6bd191c87ee0
HiromasaHori <horihiromasa.tech@gmail.com>
2022-08-13 20:04:28 +0900

    プロフィールアプリ4完了

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var profileImageView: UIImageView!
+    
+    @IBOutlet var profileCommentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        profileImageView.isHidden = true
+        profileCommentLabel.isHidden = true
     }
 
+    @IBAction func tapButton1(){
+        profileImageView.isHidden = false
+        profileCommentLabel.isHidden = false
+    }
 
 }
 
